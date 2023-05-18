e9a196ae4b5bf1562876e3693ee9ec6351bd510a
Matteo Maria Emanuele Cialei <matteome.cialei@gmail.com>
2023-05-18 15:08:06 +0200

    entity manager interface

--- include/entity/entity/entity_manager.h
+++ include/entity/entity/entity_manager.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#include <memory>
+#include <entity/entity.h>
+
+using EntityVec = std::vector<std::shared_ptr<Entity>>;
+using EntityMap = std::map<std::string, Entity>;
+
 class EntityManager {
+private:
+    EntityVec m_entities;
+    EntityMap m_entitiesMap;
+    EntityVec m_entitiesToAdd;
+
 public:
     EntityManager() = default;
     EntityManager(EntityManager &&) = default;
@@ -10,5 +21,12 @@ public:
     ~EntityManager() = default;
 
 private:
+    void init();
+
+public:
+    void update();
+    std::shared_ptr<Entity> addEntity(const std::string& tag, size_t id);
+    EntityVec & getEntities();
+    EntityVec & getEntities(const std::string & entityTag);
     
 };
